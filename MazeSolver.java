d3a5a81e5053dff6109d6ced5a06d6a34eaa407c
Chris Kontomaris <32843874+ckontomaris@users.noreply.github.com>
2018-03-08 20:14:58 -0500

    properly imports file into char array

--- MazeSolver.java
+++ MazeSolver.java
@@ -13,51 +13,45 @@ public class MazeSolver {
     public static void main(String[] args) {
         try {
             Scanner mazeFile = new Scanner(new BufferedReader(new FileReader("maze.txt")));
-            //the number of rows and columns cant be different right?
 
-            // iterate through the file to determine rows and columns
+            // we need to determine rows and columns to make an array of the maze
             int rows= 0;
             int columns= 0;
 
             //to determine # of columns, we need to see how many times we can .next() before changing lines
             //to determine # of rows, we need to see how many times we go to the next line
-            //this loops through the possible number of lines
             while(mazeFile.hasNextLine()) {
                 mazeFile.nextLine();
                 rows++;
             }
-            //resets the file
 
+            //resets the file
             mazeFile = new Scanner(new BufferedReader(new FileReader("maze.txt")));
 
             String firstRow= mazeFile.nextLine();
             //the number of columns in the first row
             columns = firstRow.length();
 
-            System.out.println(rows);
-            System.out.println(columns);
-            mazeFile.close();
-            //make an array with the same dimensions as the file
-            //and reset the file again
-            Scanner arrayFiller = new Scanner(new BufferedReader(new FileReader("maze.txt")));
-            //this is nessecary to have the scanner.next() only return a character
-            //without this it returns the whole line idk why
-            arrayFiller.useDelimiter("");
-            String[][] mazeArray = new String[rows][columns];
-
-            for(int r = 0; r<rows; r++){
-                for(int c= 0; c<columns;c++){
-                    //why would it just stop in the last row at the 6th col???
-                    mazeArray[r][c]= arrayFiller.next();
+            //now we can use the String.charat method to take each char in the file
+            mazeFile = new Scanner(new BufferedReader(new FileReader("maze.txt")));
+            //make an array
+            char[][] mazeArray = new char[rows][columns];
+            for(int r=0;r<rows;r++){
+                String row = mazeFile.nextLine();
+                for(int c=0; c<columns;c++){
+                    mazeArray[r][c]= row.charAt(c);
                 }
             }
+
             //print the maze for fun
+            System.out.println("So this is what I'm solving, eh?");
             for(int r=0; r<rows;r++){
                 for(int c=0; c<columns;c++){
                     System.out.print(mazeArray[r][c]);
                 }
+                System.out.println("");
             }
-            arrayFiller.close();
+
 
 
         } catch (Exception oof) {
